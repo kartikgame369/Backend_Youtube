103729a3900474986645108df9bcd155447939c7
kartikgame369 <kartikgame369doc@gmail.com>
2025-09-28 16:34:46 +0530

    all setup are done and server is running on 8000 and db is connected

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 import express from "express";
+// import {app} from './app.js'
 
 dotenv.config({ path: "./.env" });
 
