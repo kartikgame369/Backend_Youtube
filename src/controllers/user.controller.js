ffd1e9f8a18e74ff04119e1659440baa6c4970f2
kartikgame369 <kartikgame369doc@gmail.com>
2025-10-14 15:26:19 +0530

    pipelines are done

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import {User} from "../models/user.models.js"
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import {jwt} from "jsonwebtoken"
+import mongoose from 'mongoose';
  
 const generateAccessAndRefereshToken = async (userId) => {
   try {
@@ -344,6 +345,50 @@ if(!channel?.length){
 return res
 .status(200)
 .json(new ApiResponse(200,channel[0],"channel profile fetched successfully"))
+})
+
+const getWatchHistory = asynHandler(async(req,res)=>{
+  const user = await User.aggregate([
+    {
+      $match:{
+        _id:new mongoose . Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from:"vedios",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:[
+                {
+                  $project:{
+                    fullName:1,
+                    username:1,
+                    avatar:1
+                  }
+                },
+                {
+                  $addFields:{
+                    owner:{$first:"$owner"}
+                  }
+                }
+               
+              ]
+              
+            }
+          }
+        ]
+      }
+    }
+  ])
 })
 
 
